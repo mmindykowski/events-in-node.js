16c21c28f6085e22ab824d72516648c816a7b192
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-08-29 19:58:41 +0200

    wywołanie metody pull

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 const EventEmitter = require("events");
 
+// const emitter = new EventEmitter();
 
-const emitter = new EventEmitter();
+// console.log(emitter);
 
-console.log(emitter);
+// emitter.on("message", function (msg) {
+//   console.log(`Wiadomość: ${msg}`);
+// });
+
+// emitter.emit("message", "Czesc Programisto");
+// emitter.emit("message", "Dzien dobry!");
+
+function printData(data) {
+  console.log(`Dane ${data.data} z adresu ${data.url}`);
+}
+
+class Puller extends EventEmitter {
+  constructor(url) {
+    super();
+
+    this.url = url;
+  }
+
+  pull() {
+    this.emit("getData", {
+      data: "Pobrane dane",
+      url: this.url,
+    });
+  }
+}
+
+const puller = new Puller("https://example.com");
+
+puller.on("getData", printData);
+
+puller.pull();
